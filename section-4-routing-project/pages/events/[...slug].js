146f2568e0b9744103ba0f66bba2a2fd27afa833
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-10-05 08:51:23 -0500

    added section 4 project

---
+++ section-4-routing-project/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilterEventsPage = () => {
+  return (
+    <div>
+      <h1>Filtered events page</h1>
+    </div>
+  );
+};
+
+export default FilterEventsPage;
---
+++ section-4-routing-project/pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Events Detail Page</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
