ea5b8c8844286595de30445f83a4d6a002a2e2ca
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-11-02 23:14:45 -0500

    began server side code

--- section-8/pages/index.js
+++ section-8/pages/index.js
@@ -1,7 +1,29 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef}></input>
+        </div>
+        <div>
+          <label htmlFor="feedback" ref={feedbackInputRef}>
+            Your Feedback
+          </label>
+          <textarea id="feedback"></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
